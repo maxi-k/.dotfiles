a205275cab530256defc9f173ce26e5d030d9aa1
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-02-10 22:59:01 +0100

    Add Battery to xmobar and change layout to ResizableTall

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -9,8 +9,9 @@ import XMonad.Layout.Gaps
 import XMonad.Layout.Spacing
 import XMonad.Layout.Minimize
 import XMonad.Layout.NoBorders
+import XMonad.Layout.ResizableTile
 import XMonad.Util.EZConfig ( additionalKeys )
-import qualified Data.Map 
+import qualified Data.Map
 
 myWorkspaces = map show [1 .. 9 :: Int]
 
@@ -20,11 +21,13 @@ myLayout = tiled ||| full
         delta = 3/100 -- Percent of screen to increment when resizing
         spacingNum = 10
         full = noBorders Full
-        tiled = smartBorders $ minimize $ spacing spacingNum $ gaps  [(U, 33), (D, 10), (L, 10), (R, 10)] $ Tall nmaster delta ratio
+        tiled = smartBorders $ minimize $ spacing spacingNum $ gaps  [(U, 33), (D, 10), (L, 10), (R, 10)] $ ResizableTall nmaster delta ratio []
 
 myKeys conf@(XConfig {modMask = modm}) = Data.Map.fromList $
-  [ ((modm               , xK_p), spawn "exe=`rofi -show 'combi' -modi combi`") 
+  [ ((modm               , xK_p), spawn "exe=`rofi -show 'combi' -modi combi`")
   , ((modm .|. shiftMask , xK_p), spawn "dmenu_run")
+  , ((modm               , xK_a), sendMessage MirrorShrink)
+  , ((modm               , xK_z), sendMessage MirrorExpand)
   ]
 
 myLogHook pipe = (workspaceNamesPP xmobarPP { ppOutput = hPutStrLn pipe } >>= dynamicLogWithPP)
@@ -32,7 +35,8 @@ myLogHook pipe = (workspaceNamesPP xmobarPP { ppOutput = hPutStrLn pipe } >>= dy
   >> ewmhDesktopsLogHook
 
 myConfig pipe = defaultConfig
-  { modMask = mod4Mask -- Use Super instead of Alt
+  { terminal = "urxvt"
+  , modMask = mod4Mask -- Use Super instead of Alt
   , keys =  \c -> myKeys c `Data.Map.union` keys defaultConfig c
   , borderWidth = 2
   , normalBorderColor = "#202020"
