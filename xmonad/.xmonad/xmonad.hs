0d2c61fc6441cde3720450c5d5453378509ff651
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-02-11 00:57:56 +0100

    More work on xmobar

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -13,15 +13,37 @@ import XMonad.Layout.ResizableTile
 import XMonad.Util.EZConfig ( additionalKeys )
 import qualified Data.Map
 
-myWorkspaces = map show [1 .. 9 :: Int]
-
-myLayout = tiled ||| full
+myLayout = avoidStruts (tiled ||| full)
   where nmaster = 1 -- Default num of windows in master pane
         ratio = 1/2 -- Default ratio of master pane
         delta = 3/100 -- Percent of screen to increment when resizing
         spacingNum = 10
         full = noBorders Full
-        tiled = smartBorders $ minimize $ spacing spacingNum $ gaps  [(U, 33), (D, 10), (L, 10), (R, 10)] $ ResizableTall nmaster delta ratio []
+        tiled = smartBorders . minimize . (spacing spacingNum) $
+                gaps (map (flip (,) spacingNum) [U, D, L, R]) $
+                ResizableTall nmaster delta ratio []
+
+myStatusBar = "xmobar ~/.xmonad/xmobar.config"
+myToggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+  -- Custom PP, configure it as you like. It determines what is being written to the bar.
+myPP = xmobarPP { ppVisible = xmobarColor "#404040" ""
+                , ppCurrent = xmobarColor "#ee9a00" ""
+                , ppTitle = xmobarColor "#ee9a00" ""
+                  -- , ppHiddenNoWindows = xmobarColor "#222222" ""
+                  -- , ppLayout = xmobarColor"#790a0a" ""
+                , ppUrgent = xmobarColor "#900000" "" . wrap "[" "]"
+                , ppWsSep = "  "
+                , ppOrder = (\(workspaces:layouts:title:_) -> [workspaces, title])
+                }
+
+myWorkspaces = clickable . (map xmobarEscape) $
+  ["1 \xf108 ","2 \xf120 ","3 \xf268 ","4 \xf0e0 ","5 \xf111 ","6 \xf111 ","7 \xf111 " ,"8 \xf111 ","9 \xf001 "]
+  where clickable l = [ "<action=xdotool key super+" ++ show n ++ ">" ++ ws ++ "</action>" | (i,ws) <- zip [1..9] l,
+                        let n = i ]
+        xmobarEscape = concatMap doubleLts
+          where doubleLts '<' = "<<"
+                doubleLts x = [x]
 
 myKeys conf@(XConfig {modMask = modm}) = Data.Map.fromList $
   [ ((modm               , xK_p), spawn "exe=`rofi -show 'combi' -modi combi`")
@@ -30,20 +52,16 @@ myKeys conf@(XConfig {modMask = modm}) = Data.Map.fromList $
   , ((modm               , xK_z), sendMessage MirrorExpand)
   ]
 
-myLogHook pipe = (workspaceNamesPP xmobarPP { ppOutput = hPutStrLn pipe } >>= dynamicLogWithPP)
-  >> historyHook
-  >> ewmhDesktopsLogHook
-
-myConfig pipe = defaultConfig
+myConfig = def
   { terminal = "urxvt"
   , modMask = mod4Mask -- Use Super instead of Alt
-  , keys =  \c -> myKeys c `Data.Map.union` keys defaultConfig c
+  , keys =  \c -> myKeys c `Data.Map.union` keys def c
   , borderWidth = 2
   , normalBorderColor = "#202020"
   , focusedBorderColor = "#ffa000"
   , layoutHook = myLayout
-  , logHook = myLogHook pipe
+  -- , logHook = myLogHook pipe
   , workspaces = myWorkspaces
   }
 
-main = xmonad . myConfig =<< spawnPipe "xmobar ~/.xmonad/xmobar.config"
+main = xmonad =<< statusBar myStatusBar myPP myToggleStrutsKey myConfig
