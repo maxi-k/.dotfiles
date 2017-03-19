ac5d47b8666d28d3fa6386fba7370263c81fd51f
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-03-19 21:40:20 +0100

    Add leiningen config to dotfiles

---
+++ leiningen/.lein/profiles.clj
@@ -0,0 +1,17 @@
+{:user
+ {:dependencies [[org.clojure/tools.namespace "0.2.11"]
+                 [spyscope "0.1.5"]
+                 [criterium "0.4.3"]]
+  :injections [(require '(clojure.tools.namespace repl find))
+               ;; try/catch to work around an issue where 'lein repl'
+               ;; outside a project dir will not load reader literal
+               ;; definitions correctly
+               (try (require 'spyscope.core)
+                    (catch RuntimeException e))]
+  :plugins [[lein-exec "0.3.6"]
+            [lein-auto "0.1.3"]
+            [lein-cooper "1.2.2"]
+            [cider/cider-nrepl "0.14.0"]
+            [refactor-nrepl "2.3.0-SNAPSHOT"]]
+  }
+ :cuttle {:plugins [[lein-pprint "1.1.1"]]}}
