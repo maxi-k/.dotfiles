e03dbb1f7a1eccffd226470d8b0c785e3c95700b
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2018-09-22 21:57:35 +0200

    Add multimonitor support for i3 and polybar

--- X/.config/polybar/start.sh
+++ X/.config/polybar/start.sh
@@ -6,5 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar bottom &
+# Start on multiple monitors
+# --------------------------
+## # Get list of all monitors
+## monitors=$(xrandr | grep " connected" | awk '{ print $1 }')
+## # Launch polybar on all monitors
+## for m in $monitors
+## do
+##   MONITOR=$m polybar bottom &
+## done
+
+MONITOR=$(xrandr | grep "primary" | awk '{ print $1 }') polybar bottom &
