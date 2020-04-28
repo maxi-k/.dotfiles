226af3fef2a4b770116175d3e5eb23ea99bdaf06
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2020-04-28 18:09:58 +0200

    Start polybar on all monitors in the init script.

--- polybar/.config/polybar/start.sh
+++ polybar/.config/polybar/start.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
+mkdir -p ~/.cache/polybar
 killall -q polybar
 
 # Wait until the processes have been shut down
+echo "Stopping old polybar processes."
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Start on multiple monitors
 # --------------------------
-## # Get list of all monitors
-## monitors=$(xrandr | grep " connected" | awk '{ print $1 }')
-## # Launch polybar on all monitors
-## for m in $monitors
-## do
-##   MONITOR=$m polybar bottom &
-## done
+# Get list of all monitors
+monitors=$(xrandr | grep " connected" | awk '{ print $1 }')
+# # Launch polybar on all monitors
+echo "Found $monitors"
+for m in $monitors
+do
+  echo "Activating polybar on monitor $m"
+  MONITOR=$m polybar bottom >~/.cache/polybar/log &
+done
+echo "Activated polybar on all monitors"
 
 # MONITOR=$(xrandr | grep "primary" | awk '{ print $1 }') \
-polybar bottom &
+# polybar bottom &
