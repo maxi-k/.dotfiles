55a15054329c2bc78e21860d621b909f93be3699
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2020-06-30 09:53:57 +0200

    Make rofi use alacritty, update dwm config

--- dwm/config.h
+++ dwm/config.h
@@ -43,8 +43,8 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
@@ -79,7 +79,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return,     zoom,           {0} },
 	{ MODKEY,                       XK_Tab,        view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,          killclient,     {0} },
-	{ MODKEY,                       XK_t,          setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_e,          setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,          setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|ShiftMask,             XK_f,          setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,      setlayout,      {0} },
