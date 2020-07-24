eed0f548c86ede4c37eba2ba0dd4e665e5f9b214
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2020-07-25 01:53:27 +0200

    Add some doom more customizations.

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Maximilian Kuschewski"
+      user-mail-address "")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -29,11 +29,11 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Documents/Notes")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type nil)
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
@@ -52,3 +52,32 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;;
+
+;; Don't open the doom dashboard at startup
+;; Use the org notes file instead.
+(setq initial-buffer-choice (expand-file-name +org-capture-notes-file org-directory))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
+
+;; Import some important  keys from my own config
+(map!
+ ;; Global Keys
+ (:when (featurep! :tools magit)
+  (:prefix "C-c"
+   :desc "Git"    "g"    #'magit-status))
+ ;; Leader Keys
+ :leader
+ (:prefix "o"
+  :desc "News"   "n"    #'elfeed
+  :desc "Doom"   "d"    #'+doom-dashboard/open)
+ (:when (featurep! :editor evil)
+  (:prefix "b"
+   "v" #'evil-switch-to-windows-last-buffer
+   "c" #'cleanup-buffer)))
+
