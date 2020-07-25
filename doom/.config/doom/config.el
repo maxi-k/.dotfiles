bd9a6ded67c917f31925bc1a0c97dcf285e46ede
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2020-07-25 20:19:39 +0200

    Import window split toggle function from old config.

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -79,10 +79,38 @@
   (interactive)
   (find-file (expand-file-name +org-capture-notes-file org-directory)))
 
+(defun toggle-window-split ()
+  (interactive)
+  (when (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
+
 
 ;; Import some important  keys from my own config
 (map!
  ;; Global Keys
+ "M-O" #'toggle-window-split
+
  (:when (featurep! :tools magit)
   :desc "Git"    "C-c g"    #'magit-status)
  (:when (featurep! :emacs browse-kill-ring)
