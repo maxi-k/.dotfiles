8c1162d0d65864515c2aaaf7378280a60d398d05
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2020-08-04 09:19:02 +0200

    Make terminal-here command respect the TERMINAL env config.

--- doom/.config/doom/modules/term/terminal-here/config.el
+++ doom/.config/doom/modules/term/terminal-here/config.el
@@ -1,4 +1,8 @@
 ;;; term/terminal-here/config.el -*- lexical-binding: t; -*-
 
 (use-package! terminal-here
-  :commands (terminal-here-launch terminal-here-project-launch))
+  :commands (terminal-here-launch terminal-here-project-launch)
+  :init
+  (setq terminal-here-terminal-command
+        (list
+         (or (getenv "TERMINAL") "alacritty"))))
