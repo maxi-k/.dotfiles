157935d72acea3d02a2c03a462287b9dad64825d
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2020-08-09 13:32:39 +0200

    Test pinentry-emacs in doom config.

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -160,6 +160,10 @@
   (when (featurep! :tools lsp)
     (setq rustic-lsp-server 'rust-analyzer)))
 
+(defun pinentry-emacs (desc prompt ok error)
+  (let ((str (read-passwd (concat (replace-regexp-in-string "%22" "\"" (replace-regexp-in-string "%0A" "\n" desc)) prompt ": "))))
+    str))
+
 
 ;;;  TODO: Features to import from old config:
 ;;;
