afb272ce3d3109d89ba01231531d12ddbcc2b897
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2020-10-15 08:48:41 +0200

    Add ruby to doom config.

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -168,3 +168,7 @@
 (defun pinentry-emacs (desc prompt ok error)
   (let ((str (read-passwd (concat (replace-regexp-in-string "%22" "\"" (replace-regexp-in-string "%0A" "\n" desc)) prompt ": "))))
     str))
+
+(after! ruby
+  (when (featurep! :tools lsp)
+    (setq lsp-solargraph-use-bundler 't)))
