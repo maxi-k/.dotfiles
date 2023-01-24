f431847423aa0e0187d1cbdd6b9d228915e2a0e7
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2023-01-24 22:47:08 +0100

    Add another org-roam dashboard function.

--- doom/.config/doom/modules/ui/org-dashboard-roam/config.el
+++ doom/.config/doom/modules/ui/org-dashboard-roam/config.el
@@ -28,15 +28,35 @@
 Formatted with args: stars tag emoji tag count. XXX make this use named format strings; how does org-roam-capture-templates do it?"
   :group 'org-dashboard-roam)
 
+(defun org-dashboard-roam-symbol-as-string (s)
+  (if (symbolp s) (symbol-name s) s))
+
+(defun org-dashboard-roam-symbols-as-strings (symlist)
+  (mapcar #'org-dashboard-roam-symbol-as-string symlist))
+
 (defun org-dashboard-roam-tags (recur params)
+  "Insert headlines for tags stored in the roam database.
+Each headline links to an elisp function that opens a roam search for nodes with that tag.
+Uses emoji from `org-dashboard-roam-tag-emoji-alist' to prefix inserted headlines with.
+Params is expected to be a valid plist with the following keys (all optional):
+
+:more-than [int]
+        only include tags that have more than N nodes are associated with them (default: 0)
+:only [list-of-symbol-or-string]
+        only include tags in this list
+:exclude [list-of-symbol-or-string]
+        exclude tags from this list. combined with `org-dashboard-roam-tag-exclude-list'
+:emoji-only [bool]
+        only include tags in the output for which `org-dashboard-roam-tag-emoji-alist' has an entry
+"
   (let* ((ignored org-dashboard-roam-tag-exclude-list)
-         (ignored (append ignored (mapcar (lambda (s) (if (symbolp s) (symbol-name s) s)) (plist-get params :exclude))))
+         (ignored (append ignored (org-dashboard-roam-symbols-as-strings (plist-get params :exclude))))
          (ignored (apply #'vector ignored))
          (emoji-only (plist-get params :emoji-only))
          (having (when-let ((min-cnt (plist-get params :more-than)))
                    (list :having `(> cnt ,min-cnt))))
          (and-expr (when-let ((filter-list (plist-get params :only)))
-                     (list :and `(in tag [,@(mapcar (lambda (s) (if (symbolp s) (symbol-name s) s)) filter-list)]))))
+                     (list :and `(in tag [,@(org-dashboard-roam-symbols-as-strings filter-list)]))))
          (expr `[:select [tag (funcall count *)] :as cnt
                   :from tags
                   :where (not (in tag $v1))
@@ -59,4 +79,49 @@ Formatted with args: stars tag emoji tag count. XXX make this use named format s
         (when (or emoji (not emoji-only))
             (insert heading)
             (insert "\n"))))))
+
+
+(defun org-dashboard-roam-nodes-with-tags (recur params)
+  "Insert headlines for nodes in the roam database.
+Params is expected to be a valid plist of the following keys (required):
+
+:tags [list-of-lists-of-strings-or-symbols]
+        a list of list of tags that formulates a conjunctive normal form of tag selections, e.g:
+                ((draft wip) (project idea))
+        selects nodes that satisfy (and (or draft wip) (or project idea)),
+        e.g., drafts and work-in-progress projects and ideas.
+
+and the following optional keys
+
+:exclude-tags [bool] (default t)
+        whether to display the tags of each nodes in the headline
+"
+  (let* ((stars (make-string (+ 1 (org-current-level)) ?*))
+         (exclude-tags (plist-get params :exclude-tags))
+         (join-idx 0)
+         ;; build list of (tag-join-select-clause . tag-join-clause) cons cells
+         (join-exprs (mapcar (lambda (taglist)
+                               (setq join-idx (+ join-idx 1))
+                               `(,(format "t%i.tag" join-idx)
+                                 .
+                                 ,(concat
+                                   (format "join tags t%i on t%i.node_id = n.id and t%i.tag in (" join-idx join-idx join-idx)
+                                   (string-join
+                                    (mapcar (lambda (s)
+                                              (concat "'\"" (org-dashboard-roam-symbol-as-string s) "\"'"))
+                                            taglist)
+                                    ", ") ")")))
+                             (plist-get params :tags)))
+         (expr (concat "select n.id, n.title, " (string-join (mapcar #'car join-exprs) ", ")
+                       " from nodes n "
+                       (string-join (mapcar #'cdr join-exprs) " ")))
+         (nodes (org-roam-db-query expr))
+         (alignment (seq-reduce (lambda (m x) (max (length (cadr x)) m)) nodes 1))) ;; find max title length
+    (dolist (row nodes)
+      (let* ((id (car row))
+             (name (cadr row))
+             (tags (cddr row))
+             (tag-str (if exclude-tags "" (concat (make-string (- alignment (length name)) ?\s) ":" (string-join tags ":") ":"))))
+        (insert (format "%s [[id:%s][%s]] %s" stars id name tag-str))
+        (insert "\n")))))
 
