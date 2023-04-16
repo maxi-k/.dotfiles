bd7b15e0e35e271c39ea651c97f303c90a421342
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2023-04-16 13:40:52 +0200

    Add header line with model/system prompt info to chatgpt-shell.

--- doom/.config/doom/modules/tools/openai/config.el
+++ doom/.config/doom/modules/tools/openai/config.el
@@ -19,11 +19,46 @@
                 (add-to-list 'savehist-additional-variables 'openai-prompt-history)))))
 
 
+
+
 (use-package! chatgpt-shell
   :defer t
   :commands (chatgpt-shell chatgpt-shell-explain-code chatgpt-shell-chatgpt-prompt chatgpt-shell-send-region chatgpt-shell-send-and-review-region)
+  :init
+
+  ;; TODO move these to autoload.el without breaking header-line-format :eval
+  (defun chatgpt-shell-header-function ()
+    (let* ((left (propertize chatgpt-shell-model-version 'face 'bold))
+           (right chatgpt-shell-system-prompt)
+           (avail-width (- (window-width) (length left)))
+           (right (if (>= (+ 5 (length right)) avail-width)
+                      (string-truncate-left right (- avail-width 3))
+                    right))
+           (fmt-str (format "%%s %%%ds" avail-width)))
+      (propertize (format fmt-str left right) 'face 'mode-line-inactive)))
+
+  (defun chatgpt-shell-header--off ()
+    (interactive)
+    (setq header-line-format nil))
+
+  (defun chatgpt-shell-header--on ()
+    (interactive)
+    (if (eq major-mode 'shell-maker-mode)
+        (setq header-line-format '(:eval (funcall #'chatgpt-shell-header-function)))
+      (user-error "not in a shell buffer!")))
+
+  (define-minor-mode chatgpt-shell-header-mode
+    "Minor mode for displaying model version as a header in the
+chatgpt-shell buffer."
+    :global nil
+    :lighter "chatgpt-shell-header"
+    (if chatgpt-shell-header-mode ;; see autoloads
+        (chatgpt-shell-header--on)
+      (chatgpt-shell-header--off)))
+
+  (advice-add 'chatgpt-shell :after (lambda (&rest args) (chatgpt-shell-header-mode)))
   :config
-  (setq chatgpt-shell-model-version "gpt-4")
+  ;;(setq chatgpt-shell-model-version "gpt-4")
   (when (modulep! :lang org)
     (add-hook 'org-mode-hook (lambda () (require 'ob-chatgpt-shell)))))
 
