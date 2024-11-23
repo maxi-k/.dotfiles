e4a84c95a2f5f4bcc40ada120c3858448a04ea52
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2024-11-23 14:01:54 +0100

    Switch to gptel (instead of chatgpt-shell).

--- doom/.config/doom/modules/tools/openai/config.el
+++ doom/.config/doom/modules/tools/openai/config.el
@@ -7,75 +7,14 @@
     (spellcheck . "You are a large language model and a writing assistant. Only correct any spelling mistakes you find. Change the input text as little as possibler. Do not comment on the semantic meaning of the input. If you don't find any mistakes then reply with \"No mistakes found!\". Respond consicely.")
     (chat . "You are a large language model and a conversation partner. Respond concisely.")))
 
-(defvar myai-model-versions '("gpt-3.5-turbo" "gpt-4"))
-(defvar myai-prompt-history '())
 
-(if (bound-and-true-p savehist-loaded)
-    (add-to-list 'savehist-additional-variables 'myai-prompt-history)
-  (defvar savehist-additional-variables nil)
-  (add-hook 'savehist-mode-hook
-            (lambda ()
-              (add-to-list 'savehist-additional-variables 'myai-prompt-history))))
-
-(use-package! chatgpt-shell
-  :defer t
-  :commands (chatgpt-shell
-             chatgpt-shell-explain-code
-             chatgpt-shell-chatgpt-prompt
-             chatgpt-shell-send-region
-             chatgpt-shell-send-and-review-region
-             chatgpt-shell-prompt
-
-             myai-with-custom-system
-             myai-reset-system-prompt)
-  :init
-
-  ;; TODO move these to autoload.el without breaking header-line-format :eval
-  (defun chatgpt-shell-header-function ()
-    (let* ((left (propertize chatgpt-shell-model-version 'face 'bold))
-           (right chatgpt-shell-system-prompt)
-           (avail-width (- (window-width) (length left)))
-           (right (if (>= (+ 5 (length right)) avail-width)
-                      (if (fboundp 'string-truncate-left)
-                          (string-truncate-left right (- avail-width 5))
-                        (if (fboundp 's-truncate)
-                            (s-truncate (- avail-width 5) right)
-                          right))
-                    right))
-           (fmt-str (format "%%s %%%ds" avail-width)))
-      (propertize (format fmt-str left right) 'face 'mode-line-inactive)))
-
-  (defun chatgpt-shell-header--off ()
-    (interactive)
-    (setq header-line-format nil))
-
-  (defun chatgpt-shell-header--on ()
-    (interactive)
-    (if (eq major-mode 'shell-maker-mode)
-        (setq header-line-format '(:eval (funcall #'chatgpt-shell-header-function)))
-      (user-error "not in a shell buffer!")))
-
-  (define-minor-mode chatgpt-shell-header-mode
-    "Minor mode for displaying model version as a header in the
-chatgpt-shell buffer."
-    :global nil
-    :lighter "chatgpt-shell-header"
-    (if chatgpt-shell-header-mode ;; see autoloads
-        (chatgpt-shell-header--on)
-      (chatgpt-shell-header--off)))
-
-  (advice-add 'chatgpt-shell :after (lambda (&rest args) (chatgpt-shell-header-mode)))
-
+(use-package! gptel
+  ;; gptel-api-key set in doom/config.local.el
   :config
-
-  (setq chatgpt-shell-system-prompt (alist-get 'default myai-system-prompts))
-  (setq chatgpt-shell-model-version (car myai-model-versions))
-  (setq chatgpt-shell-request-timeout 100)
-
-  (when (modulep! :lang org)
-    (add-hook 'org-mode-hook (lambda ()
-                               ()
-                               (require 'ob-chatgpt-shell nil t)))))
-
-(use-package! dall-e-shell
-  :commands (dall-e-shell))
+  (defalias 'ai-send 'gptel-send)
+  (defalias 'ai-buffer 'gptel)
+  (defalias 'ai-add-context 'gptel-add)
+  (defalias 'ai-add-file 'gptel-add-file)
+  (defalias 'ai-org-set-topic 'gptel-org-set-topic)
+  (defalias 'ai-org-set-properties 'gptel-org-set-properties)
+  )
