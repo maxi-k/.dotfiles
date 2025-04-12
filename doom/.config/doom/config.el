2a41cdcdbe70ca40c7b83b41b03cdb57cc1cb561
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2025-04-12 16:25:57 +0200

    Minor.

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -477,13 +477,46 @@ depending on the current stat."
 (when (modulep! :tools openai)
   (map! :leader
         (:prefix ("a" . "AI")
-                 "." #'chatgpt-shell-prompt
-                 "s" #'chatgpt-shell
-                 "S" #'chatgpt-shell-send-region
-                 "p" #'myai-with-custom-system
-                 "r" #'myai-reset-system-prompt
-                 "H" #'myai-reset-prompt-history
-                 "m" #'myai-select-model)))
+                 "." #'ai-buffer
+                 "b" #'ai-buffer
+                 "s" #'ai-send
+                 "a" #'ai-add-context
+                 "f" #'ai-add-file
+                 "o" #'ai-org-set-topic
+                 "O" #'ai-org-set-properties)))
+
+(ignore
+ '(when (modulep! :tools lsp)
+   (defun lsp-booster--advice-json-parse (old-fn &rest args)
+     "Try to parse bytecode instead of json."
+     (or
+      (when (equal (following-char) ?#)
+        (let ((bytecode (read (current-buffer))))
+          (when (byte-code-function-p bytecode)
+            (funcall bytecode))))
+      (apply old-fn args)))
+   (advice-add (if (progn (require 'json)
+                          (fboundp 'json-parse-buffer))
+                   'json-parse-buffer
+                 'json-read)
+               :around
+               #'lsp-booster--advice-json-parse)
+
+   (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+     "Prepend emacs-lsp-booster command to lsp CMD."
+     (let ((orig-result (funcall old-fn cmd test?)))
+       (if (and (not test?)                             ;; for check lsp-server-present?
+                (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+                lsp-use-plists
+                (not (functionp 'json-rpc-connection))  ;; native json-rpc
+                (executable-find "emacs-lsp-booster"))
+           (progn
+             (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+               (setcar orig-result command-from-exec-path))
+             (message "Using emacs-lsp-booster for %s!" orig-result)
+             (cons "emacs-lsp-booster" orig-result))
+         orig-result)))
+   (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)))
 
 (when (modulep! :editor copilot)
   (map! "M-C" #'copilot-complete)
