2eb6f7457d68784c139196b63bb72124c89cc7dc
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2025-04-15 21:37:58 +0200

    Add emacs "vibe-mode" with copilot idle delay 0

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -521,10 +521,30 @@ depending on the current stat."
 (when (modulep! :editor copilot)
   (map! "M-C" #'copilot-complete)
   ;; set to a large idle delay so that it doesn't slow down editing by default
-  (setq copilot-idle-delay 5)
+  (defvar my/default-copilot-idle-delay 5 "The default copilot idle delay for low-latency editing.")
+  (defvar my/vibe-mode-copilot-idle-delay 0 "Idle delay for fast copilot suggestions at the cost of jankier emacs.")
+  (setq copilot-idle-delay my/default-copilot-idle-delay)
   ;; could also disable 'autocompletion (w/o calling copilot-complete) explicitly like this
   ;; (setq copilot-disable-predicates (list (lambda () t)))
-  )
+
+  ;; define 'vibe-mode', which reduces copilot idle delay; allow local and global mode
+  (define-minor-mode vibe-mode
+    "Vibe mode for fast copilot suggestions."
+    :init-value nil
+    :lighter " Vibe"
+    :global nil
+    (if vibe-mode
+        (setq-local copilot-idle-delay my/vibe-mode-copilot-idle-delay)
+      (setq-local copilot-idle-delay my/default-copilot-idle-delay)))
+  ;; convenience functions
+  (defun turn-on-vibe-mode () (interactive) (vibe-mode 1))
+  (defun turn-off-vibe-mode () (interactive) (vibe-mode -1))
+  ;; allow globalized version
+  (define-globalized-minor-mode global-vibe-mode vibe-mode
+    #'turn-on-vibe-mode
+    :init-value nil
+    :global t
+    :group 'copilot))
 
 ;; TODO is there a better way to do this built into doom?
 ;; private git-ignored configuration variables
