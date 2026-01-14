0363b01a2a16d299651e14891eb7f918a2be83d3
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2026-01-14 10:45:25 +0100

    Set default model to one w/o usage limits

--- doom/.config/doom/modules/tools/ai/config.el
+++ doom/.config/doom/modules/tools/ai/config.el
@@ -1,5 +1,7 @@
 ;;; tools/ai/config.el -*- lexical-binding: t; -*-
 
 (use-package! eca
+  :init
+  (setq eca-chat--last-known-model "github-copilot/gpt-5-mini")
   :commands (eca-transient-menu eca eca-workspace eca-restart)
   :defer t)
